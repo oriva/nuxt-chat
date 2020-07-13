ae4cc976d7b2ce955d62cd3528350a9fbe4a55c8
Oriva <weelman93@gmail.com>
2020-07-14 01:18:48 +0800

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,137 @@
+<template>
+	<section class="container">
+		<div class="row justify-content-center align-items-center height-100">
+			<div class="col-7 px-0">
+				<div class="px-4 py-5 chat-box bg-white">
+
+					<!-- Sender Message-->
+					<div class="media w-50 mb-3"><img
+							src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+							alt="user" width="50" class="rounded-circle">
+						<div class="media-body ml-3">
+							<div class="bg-light rounded py-2 px-3 mb-2">
+								<p class="text-small mb-0 text-muted">Test, which is a new approach to have</p>
+							</div>
+							<p class="small text-muted">12:00 PM | Aug 13</p>
+						</div>
+					</div>
+
+					<!-- Reciever Message-->
+					<div class="media w-50 ml-auto mb-3">
+						<div class="media-body">
+							<div class="bg-primary rounded py-2 px-3 mb-2">
+								<p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+							</div>
+							<p class="small text-muted">12:00 PM | Aug 13</p>
+						</div>
+					</div>
+
+					<div v-for="(msg, id) in arrMsg" :key="id"
+						 class="media w-50 ml-auto mb-3">
+						<div class="media-body">
+							<div class="bg-primary rounded py-2 px-3 mb-2">
+								<p class="text-small mb-0 text-white">{{ msg.msg }}</p>
+							</div>
+							<p class="small text-muted">{{ msg.date }}</p>
+						</div>
+					</div>
+
+				</div>
+
+				<!-- Typing area -->
+				<form action="#" class="bg-light">
+					<div class="input-group">
+						<input type="text" placeholder="Type a message" aria-describedby="button-addon2"
+							   class="form-control rounded-0 border-0 py-4 bg-light">
+						<div class="input-group-append">
+							<button id="button-addon2" type="submit" class="btn btn-link"><i
+									class="fa fa-paper-plane"></i></button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+
+
+    export default {
+        name: 'chat',
+        data () {
+          return {
+              nickname: 'sasa',
+              arrMsg: ''
+		  }
+		},
+        mounted: function () {
+            setInterval(()=>{
+                this.$axios.$get('http://localhost:4000/chat1')
+                    .then((response) => {
+                        // handle success
+                        this.arrMsg = response
+                        console.log(this.arrMsg)
+                    })
+                    .catch((error) => {
+                        // handle error
+                        console.log(error)
+                    })
+            },2000)
+        },
+        components: {
+
+		},
+		methods: {
+		}
+    }
+</script>
+
+<style>
+	body {
+		background-color: #74EBD5;
+		background-image: linear-gradient(90deg, #74EBD5 0%, #9FACE6 100%);
+
+		min-height:       100vh;
+	}
+
+	::-webkit-scrollbar {
+		width: 5px;
+	}
+
+	::-webkit-scrollbar-track {
+		width:      5px;
+		background: #f5f5f5;
+	}
+
+	::-webkit-scrollbar-thumb {
+		width:            1em;
+		background-color: #ddd;
+		outline:          1px solid slategrey;
+		border-radius:    1rem;
+	}
+
+	.text-small {
+		font-size: 0.9rem;
+	}
+
+	.messages-box,
+	.chat-box {
+		height:     510px;
+		overflow-y: scroll;
+	}
+
+	.rounded-lg {
+		border-radius: 0.5rem;
+	}
+
+	input::placeholder {
+		font-size: 0.9rem;
+		color:     #999;
+	}
+
+	.height-100 {
+		height: 100vh;
+	}
+</style>
+
